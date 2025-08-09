859baa1e41d2ecb83464b23e2a3357e5b00d2a28
Anjit Kandel <153750284+PhysicsDaily@users.noreply.github.com>
2025-08-09 10:24:49 +0545

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import '../styles/globals.css';
 import Script from 'next/script'; // Import the Script component
 
 function MyApp({ Component, pageProps }) {
-  // Your actual tracking IDs have been added below
   const GA_TRACKING_ID = 'G-XN081SR2KP';
   const GOOGLE_ADS_CLIENT_ID = 'ca-pub-4062746224225625';
 
@@ -32,6 +31,17 @@ function MyApp({ Component, pageProps }) {
         strategy="afterInteractive"
         src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${GOOGLE_ADS_CLIENT_ID}`}
         crossOrigin="anonymous"
+      />
+
+      {/* --- MathJax Script for LaTeX Rendering --- */}
+      <Script
+        strategy="afterInteractive"
+        src="https://polyfill.io/v3/polyfill.min.js?features=es6"
+      />
+      <Script
+        id="mathjax"
+        strategy="afterInteractive"
+        src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"
       />
 
       <Component {...pageProps} />
