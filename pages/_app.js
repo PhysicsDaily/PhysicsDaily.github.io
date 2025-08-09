7486c907fa79e8acd06f2184e31415a401e52002
Anjit Kandel <153750284+PhysicsDaily@users.noreply.github.com>
2025-08-09 13:20:16 +0545

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
+// pages/_app.js
+
 import '../styles/globals.css';
-import Script from 'next/script'; // Import the Script component
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
   const GA_TRACKING_ID = 'G-XN081SR2KP';
@@ -32,11 +34,6 @@ function MyApp({ Component, pageProps }) {
         src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${GOOGLE_ADS_CLIENT_ID}`}
         crossOrigin="anonymous"
       />
-      
-      {/* --- MathJax Scripts --- */}
-      <Script src="/mathjax-config.js" strategy="beforeInteractive" />
-      <Script src="https://polyfill.io/v3/polyfill.min.js?features=es6" strategy="beforeInteractive" />
-      <Script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js" strategy="beforeInteractive" />
 
       <Component {...pageProps} />
     </>
