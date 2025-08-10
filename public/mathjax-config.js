26354032feb2853aa9d7c314e841d6f3673801a2
PhysicsDaily <kandelanjit555@mailcom>
2025-08-10 17:49:57 +0545

    revert

--- public/mathjax-config.js
+++ public/mathjax-config.js
@@ -3,19 +3,9 @@ window.MathJax = {
     inlineMath: [['$', '$'], ['\\(', '\\)']],
     displayMath: [['$$', '$$'], ['\\[', '\\]']],
     processEscapes: true,
-    processEnvironments: true,
-    packages: {'[+]': ['ams', 'newcommand', 'configmacros']}
+    processEnvironments: true
   },
   options: {
-    skipHtmlTags: ['script', 'noscript', 'style', 'textarea', 'pre'],
-    ignoreHtmlClass: 'tex2jax_ignore',
-    processHtmlClass: 'tex2jax_process'
-  },
-  startup: {
-    pageReady: () => {
-      return MathJax.startup.defaultPageReady().then(() => {
-        console.log('MathJax initial typesetting complete');
-      });
-    }
+    skipHtmlTags: ['script', 'noscript', 'style', 'textarea', 'pre']
   }
 };
