8c190433e3e79aae8169f23708a577e9a6194dc9
PhysicsDaily <kandelanjit555@mailcom>
2025-08-10 17:48:29 +0545

    update

--- pages/_app.js
+++ pages/_app.js
@@ -8,8 +8,8 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
   {/* --- MathJax (must load before any page uses MathJax) --- */}
-  <Script src="/mathjax-config.js" strategy="afterInteractive" />
-  <Script id="MathJax-script" src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js" strategy="afterInteractive" />
+  <Script src="/mathjax-config.js" strategy="beforeInteractive" />
+  <Script id="MathJax-script" src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js" strategy="beforeInteractive" />
       {/* --- Google Analytics Script (Lazy Loaded) --- */}
       <Script
         strategy="lazyOnload"
